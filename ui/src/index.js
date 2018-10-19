7eb85ff107da34920d00e55912da0b18e2459af6
Rodrigo Brito <rig.10@hotmail.com>
2018-10-18 22:30:58 -0300

    include react router and .env

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <Route path="/:repository*" component={App} />
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
