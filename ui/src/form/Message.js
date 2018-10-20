9e7eef5a713a5b85946eafdd7c4a645951f541a1
Rodrigo Brito <rig.10@hotmail.com>
2018-10-20 19:26:38 -0300

    Include projects on user feedback

--- ui/src/form/Message.js
+++ ui/src/form/Message.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
-export default ({name, photo, content, date}) => {
+export default ({name, photo, content, date, project}) => {
   return (
     <div className="card">
       <div className="card-content">
         <div className="media">
           <div className="media-left">
             <figure className="image is-48x48">
-              <img src={photo} alt="User photo" />
+              <img src={photo} alt="User" />
             </figure>
           </div>
           <div className="media-content">
             <p className="title is-4">{name}</p>
-            <p className="subtitle is-6"><time dateTime={date}>{date.toLocaleString()}</time></p>
+            <p className="subtitle is-6"><time dateTime={date}>{date.toLocaleString()}</time> - seeing <Link to={`/${project}`}>{project}</Link></p>
           </div>
         </div>
 
