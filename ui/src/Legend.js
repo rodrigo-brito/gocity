f7ccb73f49290b41eb772c55a52d30a6b6823054
Rodrigo Brito <rig.10@hotmail.com>
2018-10-27 20:38:29 -0300

    hide some components on mobile version

--- ui/src/Legend.js
+++ ui/src/Legend.js
@@ -2,7 +2,7 @@ import React from "react";
 import legend from "./img/legend.png"
 export default () => {
   return (
-    <div className="legend">
+    <div className="legend is-hidden-mobile">
       <img src={legend} alt=""/>
     </div>
   );
