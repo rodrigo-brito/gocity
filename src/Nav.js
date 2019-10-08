436996a79d520c18abb5ca7385b123ff0bec9414
Ellison Leão <ellisonleao@gmail.com>
2019-10-08 00:51:25 -0300

    Initial Badge generator implementation. Ref #16

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,7 @@
-import React from "react";
-import logo from "./img/logo.png"
+import React from 'react';
+import logo from './img/logo.png';
+import { logoBase64 } from './utils.js';
+
 export default () => {
   return (
     <div className="level">
@@ -17,25 +19,25 @@ export default () => {
       </div>
       <div className="level-right">
         <div className="level-item is-hidden-mobile">
-          <a
-            className="github-button"
-            href="https://github.com/rodrigo-brito/gocity"
-            data-size="large"
-            data-show-count="true"
-            aria-label="Star gocity on GitHub"
-          >
-            Star
+          <a href="https://github.com/rodrigo-brito/gocity">
+            <img
+              alt="GitHub stars"
+              src="https://img.shields.io/github/stars/rodrigo-brito/gocity?style=for-the-badge&logo=github"
+            />
           </a>
           <span className="m-l-10" />
-          <a
-            className="github-button"
-            href="https://github.com/rodrigo-brito/gocity/fork"
-            data-icon="octicon-repo-forked"
-            data-show-count="true"
-            data-size="large"
-            aria-label="Fork gocity on GitHub"
-          >
-            Fork
+          <a href="https://github.com/rodrigo-brito/gocity/fork">
+            <img
+              alt="GitHub forks"
+              src="https://img.shields.io/github/forks/rodrigo-brito/gocity?label=Fork&style=for-the-badge&logo=github"
+            />
+          </a>
+          <span className="m-l-10" />
+          <a>
+            <img
+              alt="Add gocity badge for your go repo"
+              src={`https://img.shields.io/static/v1?label=gocity&message=rodrigo-brito/gocity&color=blue&style=for-the-badge&logo=${logoBase64()}`}
+            />
           </a>
         </div>
       </div>
