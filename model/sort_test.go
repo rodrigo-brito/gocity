802a19ae5700ae58485cf30aa8eea578180d97c0
billcobbler <william.shoemaker@gmail.com>
2020-10-04 01:05:54 -0500

    add sort tests

---
+++ model/sort_test.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestNodeSortByWidth(t *testing.T) {
+	nodes := []*Node{
+		{
+			Name:  "second",
+			Width: 10.1,
+		},
+		{
+			Name:  "third",
+			Width: 10.2,
+		},
+		{
+			Name:  "first",
+			Width: 1,
+		},
+	}
+
+	sort.Sort(byWidth(nodes))
+	assert.Equal(t, nodes[0].Name, "first")
+	assert.Equal(t, nodes[1].Name, "second")
+	assert.Equal(t, nodes[2].Name, "third")
+}
