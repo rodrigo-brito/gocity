adb3cbe1615eb261ccd6fc5e76adda2604d97936
julio.cesar <julio.cesar@upnid.com>
2020-10-19 22:34:19 -0300

    refactor: remove global variable, remove method and now use testify

--- handle/middlewares/cors_test.go
+++ handle/middlewares/cors_test.go
@@ -5,54 +5,49 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-var GetCorsHeadersRequest = map[string]string {
-	"Origin":                        "http://foobar.com",
-	"Access-Control-Request-Method": "GET",
-	"Access-Control-Request-Headers": "Accept, Authorization, Content-Type",
-	"Access-Control-Allow-Methods": "*",
-}
-
-var GetCorsHeadersResponse = map[string]string {
-	"Vary":                        "Origin",
-	"Access-Control-Allow-Origin": "http://foobar.com",
-	"Access-Control-Request-Headers": "Accept, Authorization, Content-Type",
-	"Access-Control-Allow-Credentials": "true",
-}
-
-var allHeaders = []string{
-	"Vary",
-	"Access-Control-Allow-Origin",
-	"Access-Control-Allow-Methods",
-	"Access-Control-Allow-Headers",
-	"Access-Control-Allow-Credentials",
-	"Access-Control-Max-Age",
-}
-
-func assertHeaders(t *testing.T, resHeaders http.Header, expHeaders map[string]string) {
-	for _, name := range allHeaders {
-		got := strings.Join(resHeaders[name], ", ")
-		want := expHeaders[name]
-		if got != want {
-			t.Errorf("Response header %q = %q, want %q", name, got, want)
-		}
-	}
-}
-
 func TestGetCors(t *testing.T) {
+
+	var getCorsHeadersRequest = map[string]string{
+		"Origin":                         "http://foobar.com",
+		"Access-Control-Request-Method":  "GET",
+		"Access-Control-Request-Headers": "Accept, Authorization, Content-Type",
+		"Access-Control-Allow-Methods":   "*",
+	}
+
+	var getCorsHeadersResponse = map[string]string{
+		"Vary":                             "Origin",
+		"Access-Control-Allow-Origin":      "http://foobar.com",
+		"Access-Control-Request-Headers":   "Accept, Authorization, Content-Type",
+		"Access-Control-Allow-Credentials": "true",
+	}
+
+	var allHeaders = []string{
+		"Vary",
+		"Access-Control-Allow-Origin",
+		"Access-Control-Allow-Methods",
+		"Access-Control-Allow-Headers",
+		"Access-Control-Allow-Credentials",
+		"Access-Control-Max-Age",
+	}
+
 	middleware := GetCors("*")
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){})
+	req, _ := http.NewRequest("GET", "http://example.com/foo", nil)
 
-	req , _ := http.NewRequest("GET","http://example.com/foo", nil)
-	for name, value := range GetCorsHeadersRequest {
-		req.Header.Add(name,value)
+	for name, value := range getCorsHeadersRequest {
+		req.Header.Add(name, value)
 	}
 
 	res := httptest.NewRecorder()
 	middleware.Handler(handler).ServeHTTP(res, req)
 
-	assertHeaders(t, res.Header(), GetCorsHeadersResponse)
+	for _, name := range allHeaders {
+		require.Equal(t, getCorsHeadersResponse[name], strings.Join(res.Header()[name], ", "))
+	}
 
 }
